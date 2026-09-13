4eb4a33e1394e37d4a49a8012890203ee5889792
Daniel Amaral <daniel.amaral.dos.santos.neto@gmail.com>
2026-09-13 13:33:29 -0300

    Bosses por nível

--- src/components/BossCard.jsx
+++ src/components/BossCard.jsx
@@ -3,23 +3,48 @@ import { useEffect, useState } from "react";
 export default function BossCard() {
   const [hp, setHp] = useState(10000);
 
+  const xpTotal =
+    Number(localStorage.getItem("ares_xp_total")) || 0;
+
+  let bossNome = "🐺 Lobo Sombrio";
+  let bossHpMax = 10000;
+
+  if (xpTotal >= 1000) {
+    bossNome = "👹 Minotauro";
+    bossHpMax = 20000;
+  }
+
+  if (xpTotal >= 3000) {
+    bossNome = "🐉 Dragão Ancião";
+    bossHpMax = 50000;
+  }
+
+  if (xpTotal >= 5000) {
+    bossNome = "⚔️ Ares";
+    bossHpMax = 100000;
+  }
+
   useEffect(() => {
     const hpSalvo =
       Number(localStorage.getItem("ares_boss_hp")) ||
-      10000;
+      bossHpMax;
 
     setHp(hpSalvo);
   }, []);
 
-  const porcentagem = (hp / 10000) * 100;
+  const porcentagem =
+    (hp / bossHpMax) * 100;
 
   return (
     <div className="card">
-      <h2>👹 Boss Atual</h2>
+
+      <h2>👹 {bossNome}</h2>
 
       <p>🎯 Projeto Tanquinho São Paulo</p>
 
-      <p>❤️ HP: {hp} / 10000</p>
+      <p>
+        ❤️ HP: {hp} / {bossHpMax}
+      </p>
 
       <div
         style={{
@@ -44,26 +69,7 @@ export default function BossCard() {
       <p style={{ marginTop: "10px" }}>
         🏆 Recompensa: +500 XP
       </p>
+
     </div>
   );
-  const xpTotal =
-  Number(localStorage.getItem("ares_xp_total")) || 0;
-
-let bossNome = "🐺 Lobo Sombrio";
-let bossHpMax = 10000;
-
-if (xpTotal >= 1000) {
-  bossNome = "👹 Minotauro";
-  bossHpMax = 20000;
-}
-
-if (xpTotal >= 3000) {
-  bossNome = "🐉 Dragão Ancião";
-  bossHpMax = 50000;
-}
-
-if (xpTotal >= 5000) {
-  bossNome = "⚔️ Ares";
-  bossHpMax = 100000;
-}
 }
